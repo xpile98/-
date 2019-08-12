b2864ae91f88f934c6278137f704d10fb0ea3048
Yongwon Cho <yongwoncho.me@gmail.com>
2019-08-12 10:17:42 +0900

    Algorithm Practicing

---
+++ 2557/10171.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "\\    /\\ \n" << endl;
+	cout << " )  (\ ') \n" << endl;
+	cout << "(  /  ) \n" << endl;
+	cout << " \\(__)| " << endl;
+	return 0;
+}
